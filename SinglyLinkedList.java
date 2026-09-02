eab82e4f9f2b5757f87ea9cae6aa5ccb7640b42f
Chua Ruy Han <chuaruyhan@gmail.com>
2026-09-02 19:43:00 +0800

    6

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -109,21 +109,21 @@ public class SinglyLinkedList<E> {
     }
 
     public void reverse(){       
-        if (isEmpty()) return;
-        Node<E> prev = null;
-        Node<E> curr = head;
-        Node<E> next = head.getNext();
-    
-        tail = curr;
+        if (head != null || size == 0) {
+            Node<E> prev = null;
+            Node<E> curr = head;
+            Node<E> next = head.getNext();
+            
+            head = tail;
+            tail = curr;
 
-        while (curr != null) {
-            next = curr.getNext();
-            curr.setNext(prev);
-            prev = curr;
-            curr = next;
+            while (curr != null) {
+                next = curr.getNext();
+                curr.setNext(prev);
+                prev = curr;
+                curr = next;
+            }
         }
-
-        head = prev;
-        
     }
 }
+    
